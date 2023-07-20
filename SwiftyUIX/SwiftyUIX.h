53942eefa95b3fa633063dc067493278f5125ab8
Moksh <suthermukesh@gmail.com>
2023-07-20 22:57:17 +0530

    Initial project setup

---
+++ SwiftyUIX/SwiftyUIX.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyUIX.h
+//  SwiftyUIX
+//
+//  Created by Moksh Suthar on 20/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyUIX.
+FOUNDATION_EXPORT double SwiftyUIXVersionNumber;
+
+//! Project version string for SwiftyUIX.
+FOUNDATION_EXPORT const unsigned char SwiftyUIXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUIX/PublicHeader.h>
+
+
---
+++ swiftyUIXExamples/swiftyUIXExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftyUIXExamplesApp.swift
+//  swiftyUIXExamples
+//
+//  Created by Moksh Suthar on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftyUIXExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
