ebc322290572a09a2bb395d3e177415b4072eda1
Moksh <suthermukesh@gmail.com>
2026-02-01 00:15:17 +0530

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyUIX",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SwiftyUIX",
+            targets: ["SwiftyUIX"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SwiftyUIX",
+            path: "SwiftyUIX",
+            sources: ["Classes"],
+            publicHeadersPath: "."
+        )
+    ]
+)
